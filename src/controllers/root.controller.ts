4e414b1947d33787270eb6de4462d8aacfc41c47
Strikehawk <strikehawk@hotmail.com>
2018-05-06 12:57:38 +0200

    setup landing page

--- src/controllers/root.controller.ts
+++ src/controllers/root.controller.ts
@@ -3,10 +3,14 @@ import { Application, Request, Response } from "express";
 import { ServiceCatalog } from "../services/service-catalog";
 
 export let registerRoutes = (app: Application, catalog: ServiceCatalog) => {
-    app.get("/", (req: Request, res: Response) => {
-      res.render("home", {
-        title: "Home",
-        screen: "home"
-      });
+  app.get("/", (req: Request, res: Response) => {
+    res.render("home", {
+      title: "Home",
+      screen: "home",
+      options: catalog.configService.options,
+      layers: catalog.layerService.getLayers().map(o => o.serialize()),
+      tileMatrixSets: catalog.tileMatrixSetService.getTileMatrixSets().map(o => o.serialize()),
+      mimeTypes: catalog.mimeTypeService.getMimeTypes()
     });
+  });
 };
